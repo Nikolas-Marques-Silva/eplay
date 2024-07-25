d24155d54b3b6fb2afbefb6d74c2b9d7b39a4f37
Nikolas-Marques-Silva <nikolas.marques.da.silva1@gmail.com>
2024-07-25 14:58:49 -0300

    feat(modulo_38):Ajuste Header Responsivo no mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,12 @@
-import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
+import {
+  HeaderBar,
+  Links,
+  LinkItem,
+  LinkCart,
+  Hamburguer,
+  HeaderRow,
+  NavMobile
+} from './styles'
 import { Link } from 'react-router-dom'
 
 import logo from '../../assets/images/logo.svg'
@@ -7,10 +15,12 @@ import carrinho from '../../assets/images/carrinho.svg'
 import { open } from '../../store/reducers/cart'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
+import { useState } from 'react'
 
 const Header = () => {
   const dispatch = useDispatch()
   const { items } = useSelector((state: RootReducer) => state.cart)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const openCart = () => {
     dispatch(open())
@@ -18,27 +28,49 @@ const Header = () => {
 
   return (
     <HeaderBar>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="EPLAY logo" />
-        </Link>
-        <nav>
-          <Links>
-            <LinkItem>
-              <Link to="/categorias">Categorias</Link>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Novidades</a>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Promoções</a>
-            </LinkItem>
-          </Links>
-        </nav>
-      </div>
-      <LinkCart href="#" onClick={openCart}>
-        {items.length} - Produto(s) <img src={carrinho} alt="Carrinho" />
-      </LinkCart>
+      <HeaderRow>
+        <div>
+          <Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </Hamburguer>
+          <Link to="/">
+            <img src={logo} alt="EPLAY logo" />
+          </Link>
+          <nav className={isMenuOpen ? 'is-open' : ''}>
+            <Links>
+              <LinkItem>
+                <Link to="/categorias">Categorias</Link>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Novidades</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Promoções</a>
+              </LinkItem>
+            </Links>
+          </nav>
+        </div>
+        <LinkCart href="#" onClick={openCart}>
+          {items.length}
+          <span> - Produto(s)</span>
+          <img src={carrinho} alt="Carrinho" />
+        </LinkCart>
+      </HeaderRow>
+      <NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <Links>
+          <LinkItem>
+            <Link to="/categorias">Categorias</Link>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </NavMobile>
     </HeaderBar>
   )
 }
